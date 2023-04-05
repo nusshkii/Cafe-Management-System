531a63b2b69402fa42ddb6371dd93d89e4eec778
Anushka <asatpute@andrew.cmu.edu>
2023-04-05 16:26:56 +0300

    sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,39 @@ class ApplicationController < ActionController::Base
 
     
     private
+     # Handling authentication
+    def current_user
+      # ||= "double-pipe equals": it checks first if a current_user is created (not nil) and creates it only itf it is nil.
+      #  We go to the session hash, if in fact the user_id is in the session hash, then go ahead and define the current user by that id.
+      @current_user ||= Employee.find(session[:employee_id]) if session[:employee_id]
+    end
+    
+    # We also want to make this a helper method so that our views can access it later. 
+    helper_method :current_user
+    
+    # A method which simply tells us if a user is logged in (true if you have a user_id in session hash, i.e., a current_user). 
+    def logged_in?
+      current_user
+    end
+    helper_method :logged_in?
+    
+    # Finally, we will have a method called check_login that we can use as an additional before_action in other controllers.
+    def check_login
+      redirect_to login_path, alert: "You need to log in to view this page." if current_user.nil?
+    end
+
+    # just show a flash message instead of full CanCan exception
+    rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = "You are not authorized to take this action.  Go away or I shall taunt you a second time."
+    redirect_to home_path
+    end
+
+    # handle 404 errors with an exception as well
+    rescue_from ActiveRecord::RecordNotFound do |exception|
+    flash[:error] = "Seek and you shall find... but not this time"
+    redirect_to home_path
+    end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,23 +4,59 @@ Rails.application.routes.draw do
     namespace :v1 do
       # provide the routes for the API here
 
-      
-      
+
 
     end
   end
 
   # Routes for regular HTML views go here...
+  # Semi-static page routes
+  get 'home', to: 'home#index', as: :home
   
-
-
-
-
-
-
-
-
-  # You can have the root of your site routed with 'root'
+  # Employee Routes
+  get '/employees', to: 'employees#index', as: 'employees' 
+  post '/employees' , to: 'employees#create'
+  get '/employees/new', to: 'employees#new', as: 'new_employee' 
+  get 'employees/:id/edit', to: 'employees#edit', as: 'edit_employee' 
+  get '/employees/:id', to: 'employees#show', as: 'employee'
+  patch '/employees/:id', to: 'employees#update' 
+  put '/employees/id', to: 'employees#update' 
+  delete '/employees/:id', to: 'employees#destroy'
+  
+        
+  # Authentication routes
+  get '/login', to: 'sessions#new', as: 'login' 
+  get '/logout', to: 'sessions#destroy', as: 'logout'
+        
+  get '/sessions', to: 'sessions#index', as: 'sessions' 
+  post '/sessions', to: 'sessions#create' 
+  get '/sessions/new', to: 'sessions#new' , as: 'new_session' 
+  get '/sessions/:id/edit', to: 'sessions#edit', as: 'edit_session'
+  get '/sessions/:id', to: 'sessions#show', as: 'session' 
+  patch '/sessions/:id', to: 'sessions#update'
+  put '/sessions/:id', to: 'sessions#update' 
+  delete 'sessions/:id', to: 'sessions#destroy'
+        
+  # Store Routes
+  get '/stores', to: 'stores#index', as: 'stores' 
+  post '/stores' , to: 'stores#create'
+  get '/stores/new', to: 'stores#new', as: 'new_store' 
+  get '/stores/:id/edit', to: 'stores#edit', as: 'edit_store' 
+  get '/stores/:id', to: 'stores#show', as: 'store'
+  patch '/stores/:id', to: 'stores#update' 
+  put '/stores/:id', to: 'stores#update'
+  
+  # Assignment Routes
+  get '/assignments', to: 'assignments#index', as: 'assignments' 
+  post '/assignments', to: 'assignments#create' 
+  get '/assignments/new', to: 'assignments#new', as: 'new_assignment'
+  get 'assignments/:id/edit' ,to: 'assignments#edit', as: 'edit_assignment' 
+  get '/assignments/:id', to: 'assignments#show', as: 'assignment'
+  patch '/assignments/:id', to: 'assignments#update'
+  delete '/assignments/:id', to: 'assignments#destroy'  
   
   
 end
+
+
+
